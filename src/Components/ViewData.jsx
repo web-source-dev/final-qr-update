1eeb6990788bc61e351cd41d35dd14b5f82f9205
web-source-dev <muhammadnouman72321@gmail.com>
2024-11-14 14:26:48 +0500

    Update ViewData.jsx

--- src/Components/ViewData.jsx
+++ src/Components/ViewData.jsx
@@ -216,7 +216,7 @@ const ViewData = () => {
                       onClick={() => handleQRCodeClick(`qr-code-canvas-${user._id}`)} // Open QR in popup
                     />
                     <button className="all-users-page-download-btn-qr" onClick={() => downloadQRCode(user.name, user._id)}>
-                      <i className="ri-download-line"></i>
+                      Download <i className="ri-download-line"></i>
                     </button>
                   </div>
                 )}
